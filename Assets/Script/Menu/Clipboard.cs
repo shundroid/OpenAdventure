5fd14b8b58c4e1b13b2aec9564befedbe8839385
shundroid <ra24483@qb3.so-net.ne.jp>
2016-01-01 14:36:06 +0900

    add Clipboard System

---
+++ Assets/Script/Menu/Clipboard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clipboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Click()
+    {
+        Debug.Log(GUIUtility.systemCopyBuffer);
+    }
+}
